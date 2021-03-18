cef22744616997a156bd29c30c852ca9d96b207e
Réka Sallai <reka.sallai@stud.uni-corvinus.hu>
2021-03-18 11:21:09 +0100

    commit all

--- HajósTeszt/wwwroot/jsgyak.js
+++ HajósTeszt/wwwroot/jsgyak.js
@@ -1,1 +1,13 @@
-﻿
+﻿window.onload = function () {
+    consol.log("Start!");
+    let hova = document.getElementById("ide");
+    for (var s = 0; s < 10; s++) {
+        let sor = document.createElement("div");
+        hova.appendChild(sor)
+        for (var o = 0; o < 10; o++) {
+            let szam = document.createElement("div");
+            szam.innerText = (s + 1) * (o + 1);
+            sor.appendChild(szam);
+        }
+    }
+}
