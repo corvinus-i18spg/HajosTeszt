2e34630ad2466ed7bfef75fde779d2289f468d0a
Réka Sallai <reka.sallai@stud.uni-corvinus.hu>
2021-03-18 11:41:29 +0100

    commit all

--- HajósTeszt/wwwroot/jsgyak.js
+++ HajósTeszt/wwwroot/jsgyak.js
@@ -3,10 +3,12 @@
     let hova = document.getElementById("ide");
     for (var s = 0; s < 10; s++) {
         let sor = document.createElement("div");
+        sor.classList.add("sor")
         hova.appendChild(sor);
 
         for (var o = 0; o < 10; o++) {
             let szam = document.createElement("div");
+            szam.classList.add("elem")
             szam.innerText = (s + 1) * (o + 1);
             sor.appendChild(szam);
         }
