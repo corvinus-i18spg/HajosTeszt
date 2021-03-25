7f59fc6722b6161183fa4f35f047c9112e441c2f
Réka Sallai <reka.sallai@stud.uni-corvinus.hu>
2021-03-25 11:04:42 +0100

    commit all

--- HajósTeszt/wwwroot/hetedik.js
+++ HajósTeszt/wwwroot/hetedik.js
@@ -1,1 +1,15 @@
-﻿
+﻿var kerdesek;
+
+fetch('/question.json').then(response => response.json())
+    .then(data => letöltésBefejeződött(data)
+    );
+window.onload = function() {
+    function letöltésBefejeződött(d) {
+        console.log("Sikeres letöltés")
+        console.log(d)
+        kérdések = d
+    };
+}
+
+
+
