83a1eeef703aac65cbae01dc6cfab3ea5582a1ed
Réka Sallai <reka.sallai@stud.uni-corvinus.hu>
2021-04-22 10:06:43 +0200

    commit all

--- HajósTeszt/Controllers/BoatController.cs
+++ HajósTeszt/Controllers/BoatController.cs
@@ -12,13 +12,16 @@ namespace HajósTeszt.Controllers
     {
 
         [HttpGet]
-        [Route("questions/all")]
-        public ActionResult M1()
+        [Route("questions/{sorszám}")]
+        public ActionResult M2(int sorszám)
         {
             HajostesztContext context = new HajostesztContext();
-            var kérdések = from x in context.Questions select x.QuestionText;
+            var kérdés = (from x in context.Questions 
+                         where x.QuestionId == sorszám
+                         select x).FirstOrDefault();
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
 
-            return new JsonResult(kérdések);
+            return new JsonResult(kérdés);
         }
         public IActionResult Index()
         {
