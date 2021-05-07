96e64ea58745de48f8ea9b65dd46215c9d51c5e7
Réka Sallai <reka.sallai@stud.uni-corvinus.hu>
2021-05-07 15:01:55 +0200

    commit all

---
+++ HajósTeszt/Controllers/QuestionController.cs
@@ -0,0 +1,25 @@
+﻿using HajósTeszt.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajósTeszt.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/count")]
+
+        public int M1() 
+        {
+            HajostesztContext context = new HajostesztContext();
+            int kerdesekSzama = context.Questions.Count();
+            return kerdesekSzama;
+        }
+    }
+}
