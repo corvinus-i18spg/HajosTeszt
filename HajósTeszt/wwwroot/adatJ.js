74e3099085aaa45acd37a12a318ead00d6ec6fa9
Réka Sallai <reka.sallai@stud.uni-corvinus.hu>
2021-05-24 14:13:23 +0200

    commit all

---
+++ HajósTeszt/wwwroot/adatJ.js
@@ -0,0 +1,10 @@
+﻿window.onload = function () {
+    let hova = document.getElementById("adat");
+    for (var s = 1; s < 10; s++) {
+        let sor = document.createElement("div");
+        let szoveg = document.createElement("p");
+        sor.classList.add("sor")
+        szoveg.classList.add("p")
+        hova.appendChild(sor);
+        hova.appendChild(szoveg);
+}
